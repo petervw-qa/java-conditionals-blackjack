a61219ab1808baa834a1e3c7c3a7209a23bea8de
Tukanami <67924806+Tukanami@users.noreply.github.com>
2021-01-07 15:11:05 +0000

    first commit

---
+++ src/com/qa/main/Methods.java
@@ -0,0 +1,24 @@
+package com.qa.main;
+
+public class Methods {
+	
+	// playBlackjack Method
+	public int playBlackjack(int num1,int num2) {
+		if (num1 > 21 && num2 > 21) {
+			return 0;
+		} else if (num1 > 21 || num2 > 21) {
+			if (num1 > 21) {
+				return num2;
+			} else {
+				return num1;
+			} 
+		} else {
+			if ((21 - num1) < (21 - num2)) {
+				return num1;
+			} else {
+				return num2;
+			}
+		}
+		
+	}
+}
---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,13 @@
+package com.qa.main;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		 
+		Methods game1 = new Methods();
+		
+		System.out.println(game1.playBlackjack(18, 6));
+		
+	}
+
+}
